56ead19d15747ed1770d1b36e6c35f3d5c57ad04
Chaudry24 <adil_chaudry24@hotmail.com>
2022-03-30 12:32:20 -0500

    added models 16 and 17

--- models.py
+++ models.py
@@ -496,3 +496,77 @@ def model15(dense_units=1024, lr=1e-3):
                   metrics=[tf.keras.metrics.RootMeanSquaredError()])
 
     return model
+
+
+def model16(dense_units=1024, lr=1e-3):
+    """This function returns a compiled NN with skip connections"""
+
+    # input layer
+    inputs = tf.keras.layers.Input(shape=(2))
+
+    # dense layers
+    d1 = tf.keras.layers.Dense(units=dense_units, activation="relu")(inputs)
+    d2 = tf.keras.layers.Dense(units=dense_units, activation="relu")(d1)
+    d3 = tf.keras.layers.Dense(units=dense_units, activation="relu")(d2)
+    d4 = tf.keras.layers.Dense(units=dense_units, activation="relu")(d3)
+    d4 = tf.keras.layers.Dropout(0.05)(d4)
+
+    # concatenate d3 with d4 to pass into d5
+    c34 = tf.keras.layers.Concatenate()([d3, d4])
+    d5 = tf.keras.layers.Dense(units=dense_units, activation="relu")(c34)
+    d5 = tf.keras.layers.Dropout(0.05)(d5)
+
+    # pass d5 into output
+    output = tf.keras.layers.Dense(units=1)(d5)
+
+    # connect the inputs and outputs
+    model = tf.keras.Model(inputs, output)
+
+    # compile model
+    model.compile(loss=tf.keras.losses.MeanSquaredError(),
+                  optimizer=tf.keras.optimizers.Adam(learning_rate=lr),
+                  metrics=[tf.keras.metrics.RootMeanSquaredError()])
+
+    return model
+
+
+def model17(dense_units=1024, lr=1e-3):
+    """This function returns a compiled NN with skip connections"""
+
+    # input layer
+    inputs = tf.keras.layers.Input(shape=(2))
+
+    # dense layers
+    d1 = tf.keras.layers.Dense(units=dense_units, activation="relu")(inputs)
+    d2 = tf.keras.layers.Dense(units=dense_units, activation="relu")(d1)
+
+    # concat output of d1 with d2 to pass into d3
+    c12 = tf.keras.layers.Concatenate()([d1, d2])
+    d3 = tf.keras.layers.Dense(units=dense_units, activation="relu")(c12)
+
+    # concatenate output of d2 with d3 to pass into d4
+    c23 = tf.keras.layers.Concatenate()([d2, d3])
+    d4 = tf.keras.layers.Dense(units=dense_units, activation="relu")(c23)
+
+    # concatenate d3 with d4 to pass into d5
+    c34 = tf.keras.layers.Concatenate()([d3, d4])
+    d5 = tf.keras.layers.Dense(units=dense_units, activation="relu")(c34)
+    d5 = tf.keras.layers.Dropout(0.05)(d5)
+
+    # concatenate inputs with d5 and pass into output
+    cind5 = tf.keras.layers.Concatenate()([inputs, d5])
+    d6 = tf.keras.layers.Dense(units=dense_units, activation="relu")(cind5)
+    d6 = tf.keras.layers.Dropout(0.05)(d6)
+
+    # pass d5 into output
+    output = tf.keras.layers.Dense(units=1)(d6)
+
+    # connect the inputs and outputs
+    model = tf.keras.Model(inputs, output)
+
+    # compile model
+    model.compile(loss=tf.keras.losses.MeanSquaredError(),
+                  optimizer=tf.keras.optimizers.Adam(learning_rate=lr),
+                  metrics=[tf.keras.metrics.RootMeanSquaredError()])
+
+    return model
