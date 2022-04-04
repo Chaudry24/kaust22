406f37c89acbe61a96206800a7e3c22b133a0480
Chaudry24 <adil_chaudry24@hotmail.com>
2022-04-04 14:08:46 -0500

    added early stopping and validation_data to model.fit

--- sim_m20d2.py
+++ sim_m20d2.py
@@ -27,8 +27,26 @@ train_x = tf.convert_to_tensor(train_x)
 train_y = tf.convert_to_tensor(train_y)
 test = tf.convert_to_tensor(test)
 
+callbacks = [
+    tf.keras.callbacks.EarlyStopping(monitor='val_loss',
+                                     min_delta=0,
+                                     patience=0,
+                                     verbose=0,
+                                     mode='min',
+                                     baseline=None,
+                                     restore_best_weights=True)
+]
+
+# open validation data to get validation error
+with open("./data-competition-1a/val_x2.npy", mode="rb") as file:
+    val_x = tf.convert_to_tensor(np.load(file))
+with open("./data-competition-1a/val_y2.npy", mode="rb") as file:
+    val_y = tf.convert_to_tensor(np.load(file))
+
 # fit the model for 1000 epochs
-model.fit(x=train_x, y=train_y, epochs=1000)
+model.fit(x=train_x, y=train_y, epochs=1000,
+          validation_data=(val_x, val_y),
+          callbacks=callbacks)
 
 # get the predictions for test set
 preds = model.predict(x=test)
@@ -36,12 +54,6 @@ preds = model.predict(x=test)
 # save the model predictions as .npy
 with open("./results/preds_m20d2.npy", mode="wb") as file:
     np.save(file, preds)
-
-# open validation data to get validation error
-with open("./data-competition-1a/val_x2.npy", mode="rb") as file:
-    val_x = tf.convert_to_tensor(np.load(file))
-with open("./data-competition-1a/val_y2.npy", mode="rb") as file:
-    val_y = tf.convert_to_tensor(np.load(file))
 
 # get loss on validation set
 val_results = np.array(model.evaluate(x=val_x, y=val_y))
