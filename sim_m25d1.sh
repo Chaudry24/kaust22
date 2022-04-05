3b3c475ec6a2ad600821abba7eaa6ab07a2ffcf4
Chaudry24 <adil_chaudry24@hotmail.com>
2022-04-05 12:37:35 -0500

    simulaitons ready for deployment

---
+++ sim_m25d1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -J simulation
+#SBATCH -o simulation.o%j
+#SBATCH -t 24:00:00
+#SBATCH -N 1 -n 1
+#SBATCH -p gpu
+#SBATCH --gres=gpu:1
+#SBATCH --mem=64GB
+#SBATCH --mail-user=machaudry@uh.edu
+#SBATCH --mail-type=END,FAIL
+
+/project/jun/machaudr/miniconda3.9/envs/myenv/bin/python -u sim_m25d1.py
